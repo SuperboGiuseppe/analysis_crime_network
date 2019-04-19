5198f0a55c8841e5ccf5be5f3a1ffffcd8c7d4dc
SuperboGiuseppe <giuseppe.superbo97@gmail.com>
2019-04-19 12:10:27 +0300

    Drop menu' functionality added to the GUI

--- gui.py
+++ gui.py
@@ -2,7 +2,7 @@ from tkinter import *
 from tkinter import ttk
 import task1 as t1
 import matplotlib.pyplot as plt
-from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg,  NavigationToolbar2Tk
 import networkx as nx
 
 def main_window():
@@ -13,24 +13,64 @@ def main_window():
         if(clicked_tab == 0):
             draw_plot(0)
 
+    def init():
+        net_csv = "./DataSets/netWR.csv"
+        net_graph = t1.csv_to_graph(net_csv)
+        fig = plt.figure(1)
+        plt.title("Network built upon WireTap Records")
+        net_graphAttribute = t1.get_graph_attributes(net_graph)
+        nx.draw(net_graph,pos=nx.spring_layout(net_graph), with_labels=True, node_color="skyblue")
+        main.canvas = FigureCanvasTkAgg(fig, master=main)
+        main.canvas.get_tk_widget().grid(row=1, column=0, columnspan=3, sticky=NSEW)
+        toolbar_frame = Frame(main)
+        toolbar_frame.config(relief="sunken", borderwidth=1)
+        toolbar = NavigationToolbar2Tk(main.canvas, toolbar_frame)
+        toolbar_frame.grid(column=0, row=3, sticky="w")
+        toolbar.update()
+
     def draw_plot(tab):
-        if(tab == 0):
-            netWR_csv = "./DataSets/netWR.csv"
-            netWR_G = t1.csv_to_graph(netWR_csv)
+        def OptionMenu_Changed(event):
+            graph = variable.get()
+            if(graph==0, tab==0):
+                net_csv = "./DataSets/netWR.csv"
+                title = "Network built upon WireTap Records"
+                color = "skyblue"
+            if(graph==1, tab==0):
+                net_csv = "./DataSets/netAW.csv"
+                title = "Network built upon Arrest warrants"
+                color = "orange"
+            if(graph==2, tab==0):
+                net_csv= "./DataSets/netJU.csv"
+                title = "Network built upon judgment"
+            try:
+                main.canvas.get_tk_widget().destroy()
+            except:
+                pass
+            net_graph = t1.csv_to_graph(net_csv)
+            fig = plt.figure(1)
+            plt.title(title)
+            net_graphAttribute = t1.get_graph_attributes(net_graph)
             fig = plt.figure(1)
             plt.title("Network built upon WireTap Records")
-            netWR_graphAttribute = t1.get_graph_attributes(netWR_G)
-            netWR_G.remove_nodes_from(list(nx.isolates(netWR_G)))
-            nx.draw(netWR_G, with_labels=True, node_color="skyblue")
-            canvas = FigureCanvasTkAgg(fig, master=main)
-            canvas.get_tk_widget().pack(side=TOP, fill=BOTH, expand=1)
-            canvas._tkcanvas.pack(side=TOP, fill=BOTH, expand=1)
+            nx.draw(net_graph, pos=nx.spring_layout(net_graph), with_labels=True, node_color=color)
+            net_graphAttribute = t1.get_graph_attributes(net_graph)
+            main.canvas = FigureCanvasTkAgg(fig, master=main)
+            main.canvas.get_tk_widget().grid(row=1, column=0, columnspan=3, sticky=NSEW)
+            #net_graph_isolated = net_graph.remove_nodes_from(list(nx.isolates(net_graph)))
+
+        variable = StringVar(main)
+        variable.set("WireTap Records")
+        dropmenu = OptionMenu(main, variable, "WireTap Records", "Arrest warrants", "Judgment", command=OptionMenu_Changed)
+        dropmenu.grid(column=1, row=3, sticky="e")
+
+
 
 
     main = Tk()
+    main.grid_columnconfigure(0, weight=1)
     main.title("Project 4 : Analysis of a crime network")
     main.minsize(1080, 700)
-    main.resizable(width=False, height= False)
+    main.resizable(width=True, height=True)
     tabControl = ttk.Notebook(main)
     tab1 = ttk.Frame(tabControl)
     tab2 = ttk.Frame(tabControl)
@@ -44,12 +84,11 @@ def main_window():
     tabControl.add(tab4, text="Task 4")
     tabControl.add(tab5, text="Task 5")
     tabControl.add(tab6, text="Task 6")
-    tabControl.grid(row=0)
-    tabControl.pack(side=TOP, anchor=NW)
+    tabControl.grid(row=0, sticky="nw")
+    init()
     frame = Frame(main)
     frame.canvas = Canvas(main, width=900, height=650)
     draw_plot(0)
-    frame.canvas.pack(fill=BOTH, padx=10)
     main.mainloop()
 
 
